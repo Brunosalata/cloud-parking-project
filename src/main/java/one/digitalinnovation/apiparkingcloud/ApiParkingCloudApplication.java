0f30d70093b91d8132229b4e513e85e3934679e0
Bruno Salata <brunoslima@gmail.com>
2022-08-06 18:47:15 -0300

    Spring Web

---
+++ src/main/java/one/digitalinnovation/apiparkingcloud/ApiParkingCloudApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.apiparkingcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParkingCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParkingCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/apiparkingcloud/ApiParkingCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.apiparkingcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParkingCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
