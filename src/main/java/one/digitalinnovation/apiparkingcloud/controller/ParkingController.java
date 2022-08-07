e2b6a827a9186450d41bc94656def4d267177fc7
Bruno Salata <brunoslima@gmail.com>
2022-08-07 13:15:03 -0300

    Meth findById-Create and @PostMapping

--- src/main/java/one/digitalinnovation/apiparkingcloud/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/apiparkingcloud/controller/ParkingController.java
@@ -1,13 +1,13 @@
 package one.digitalinnovation.apiparkingcloud.controller;
 
+import one.digitalinnovation.apiparkingcloud.controller.dto.ParkingCreateDTO;
 import one.digitalinnovation.apiparkingcloud.controller.dto.ParkingDTO;
 import one.digitalinnovation.apiparkingcloud.controller.mapper.ParkingMapper;
 import one.digitalinnovation.apiparkingcloud.model.Parking;
 import one.digitalinnovation.apiparkingcloud.service.ParkingService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +29,21 @@ public class ParkingController {
         List<Parking> parkingList = parkingService.findAll();
         List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
         return ResponseEntity.ok(result);       //retorno vai ser um ResponseEntity.ok (retorna código 200) com result como corpo
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ParkingDTO> findById(@PathVariable String id) {
+        Parking parking = parkingService.findById(id);
+        ParkingDTO result = parkingMapper.toParkingDTO(parking);
+        return ResponseEntity.ok(result);       //retorno vai ser um ResponseEntity.ok (retorna código 200) com result como corpo
+    }
+
+    @PostMapping
+    public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO dto) {       //Recebe um objeto com menos informação (ParkingCreatedto) e retorna um objeto completo
+        var parkingCreate = parkingMapper.toParkingCreate(dto);
+        var parking = parkingService.create(parkingCreate);
+        var result = parkingMapper.toParkingDTO(parking);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
 }
--- src/main/java/one/digitalinnovation/apiparkingcloud/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/apiparkingcloud/service/ParkingService.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.apiparkingcloud.service;
 import one.digitalinnovation.apiparkingcloud.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,9 +16,12 @@ public class ParkingService {
     private static Map<String, Parking> parkingMap = new HashMap();
 
     static {
-        var id = getUUID();
-        Parking parking = new Parking(id, "CFU-9898", "SC", "ONIX", "Bordô");
-        parkingMap.put(id, parking);
+        var id1 = getUUID();
+        var id2 = getUUID();
+        Parking parking1 = new Parking(id1, "CFU-9898", "SC", "ONIX", "VINHO");
+        Parking parking2 = new Parking(id2, "DTG-7748", "CE", "PALIO", "PRATA");
+        parkingMap.put(id1, parking1);
+        parkingMap.put(id2, parking2);
     }
 
     public List<Parking> findAll() {
@@ -26,5 +30,17 @@ public class ParkingService {
 
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", ""); //pega o UUID do Java, converte para String e troca hifem por vazio
+    }
+
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
     }
 }
